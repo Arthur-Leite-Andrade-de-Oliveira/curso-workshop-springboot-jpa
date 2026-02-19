e47300fb1f3c02b335b86c7fa8415de2aa4f6339
4rthurzinhoo <arthurlaoliveira@gmail.com>
2026-02-19 18:43:54 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/treino/curso/repositories/OrderItemRepository.java
+++ src/main/java/com/treino/curso/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.treino.curso.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.treino.curso.entities.OrderItem;
+import com.treino.curso.entities.pk.OrderItemPk;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPk>{
 
 }
